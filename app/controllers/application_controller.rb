708bd9e171c03578bd2fa378755cb0a3a6616b7e
Nate DeWaele <nbdewaele@gmail.com>
2017-10-10 15:39:39 -0400

    username authenticated in controllers application and in user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+	before_action :configure_permitted_parmeters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parmeters
+		devise_parameter_sanitizer.permit(:sugn_up, keys: [:fullname])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
 end
