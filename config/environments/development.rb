c81353b7d99b47dcc7d10bc0fdfe876bc8f36d25
Nate DeWaele <nbdewaele@gmail.com>
2017-10-10 21:40:37 -0400

    devise debugged

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
 	config.action_mailer.delivery_method = :smtp
 
-	config.action_mailer.delivery_method = {
+	config.action_mailer.smtp_settings = {
 		address: 'smtp.gmail.com',
 		port: 587,
 		enable_starttls_auto: true,
